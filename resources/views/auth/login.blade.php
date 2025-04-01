3b4bf010cac00a0cf9e45e6ec1c23411bf17f43c
santiago <memesanti77@gmail.com>
2025-03-31 19:47:44 -0500

    lo que borro luis jaja :v

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 <style>
     /* Fondo con degradado azul */
     body {
-    background: linear-gradient(135deg,rgb(255, 255, 255),rgb(176, 226, 147));
+    background: linear-gradient(135deg,rgb(255, 255, 255),rgb(159, 203, 132));
     min-height: 100vh;
     display: flex;
     flex-direction: column; /* 🔹 Esto mantiene el formulario centrado */
